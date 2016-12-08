b0e6dad124619268fae0e7e051a810c20195f144
Tomasz Dziurko <tdziurko@gmail.com>
2016-12-08 17:33:56 +0100

    Move all frontend packages to frontend / public_area || admin_area

---
+++ src/main/java/com/jvm_bloggers/frontend/admin_area/counters/NewPostsCounterModel.java
@@ -0,0 +1,30 @@
+package com.jvm_bloggers.frontend.admin_area.counters;
+
+import com.jvm_bloggers.common.utils.DateTimeUtilities;
+import com.jvm_bloggers.common.utils.NowProvider;
+import com.jvm_bloggers.core.data_fetching.blog_posts.domain.BlogPostRepository;
+import org.apache.wicket.injection.Injector;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.time.LocalDateTime;
+
+public class NewPostsCounterModel extends AbstractReadOnlyModel<Integer> {
+
+    @SpringBean
+    private BlogPostRepository blogPostRepository;
+
+    @SpringBean
+    private NowProvider nowProvider;
+
+    public NewPostsCounterModel() {
+        Injector.get().inject(this);
+    }
+
+    @Override
+    public Integer getObject() {
+        LocalDateTime lastPublicationDate =
+            DateTimeUtilities.lastPublicationDate(nowProvider.now());
+        return blogPostRepository.countByPublishedDateAfter(lastPublicationDate);
+    }
+}
--- src/test/groovy/com/jvm_bloggers/MockSpringContextAwareSpecification.groovy
+++ src/test/groovy/com/jvm_bloggers/MockSpringContextAwareSpecification.groovy
@@ -1,6 +1,6 @@
 package com.jvm_bloggers
 
-import com.jvm_bloggers.admin_panel.session.UserSession
+import com.jvm_bloggers.frontend.admin_area.session.UserSession
 import org.apache.wicket.bean.validation.BeanValidationConfiguration
 import org.apache.wicket.mock.MockApplication
 import org.apache.wicket.protocol.http.WebApplication
